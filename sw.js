59c8b788134c9438925d63a7636b78a8b09d1d58
billioG <59061218+billioG@users.noreply.github.com>
2025-12-17 13:46:21 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "21dias-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
