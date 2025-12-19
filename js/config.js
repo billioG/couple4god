1a93014166f731c91f0d6f4c12cf226e21554fbc
billioG <59061218+billioG@users.noreply.github.com>
2025-12-18 19:31:27 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // REEMPLAZA CON TUS DATOS REALES DE SUPABASE
-const SUPABASE_URL = 'https://tu-proyecto.supabase.co';
-const SUPABASE_KEY = 'tu-anon-key-publica';
+const SUPABASE_URL = 'https://dsiuuymgyzkcksaqtoqk.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzaXV1eW1neXprY2tzYXF0b3FrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5NTg2NDksImV4cCI6MjA4MTUzNDY0OX0.BxxUrlixe9X-JA--G_0OUeqD5ZIDikIc2WcjcIbBamg';
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
