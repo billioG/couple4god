7fa793a023b8af22076f51cd4b558d334e0939b4
billioG <59061218+billioG@users.noreply.github.com>
2025-12-18 18:40:50 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,22 @@
-const CACHE_NAME = "c4g-v2";
+const CACHE_NAME = "c4g-v5-fix"; // Cambiamos versión para limpiar caché
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js"];
 
 self.addEventListener("install", (e) => {
+  self.skipWaiting(); // Obliga a instalarse de inmediato
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key); // Borra caché vieja
+        })
+      );
+    })
+  );
+  self.clients.claim(); // Toma control inmediato
 });
 
 self.addEventListener("fetch", (e) => {
